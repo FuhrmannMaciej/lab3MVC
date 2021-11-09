21310cbd57ea12800d4e2e37b58410f4a94ec8ef
maciej.fuhrmann <maciej.fuhrmann@happyteam.io>
2021-11-09 19:38:05 +0100

    Add project files.

---
+++ lab3MVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using lab3MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", model);
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lab3MVC/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3MVC.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Display(Name = "Ilość powtórzeń")]
+        [Range(1,5, ErrorMessage = "Proszę wpisać liczbę w przedziale od 1 do 5.")]
+        public int NumberOfRepetitions { get; set; }
+    }
+}
